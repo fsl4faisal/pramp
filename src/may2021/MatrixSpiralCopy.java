309f8ebc5a2582344f33e1c46084f7d1c4676617
faisal <faisal@pop-os.localdomain>
2021-05-01 20:14:43 +0530

    first commit

---
+++ src/may2021/MatrixSpiralCopy.java
@@ -0,0 +1,13 @@
+package may2021;
+
+class MatrixSpiralCopy {
+
+	static int[] spiralCopy(int[][] inputMatrix) {
+		// your code goes here
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
